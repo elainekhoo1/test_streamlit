0911a422e105c116b9870d90054138e9b301be6d
Elaine K <elaine.khoosynnyie@petronas.com>
2023-10-06 16:31:58 +0800

    Include random pick as lucky number from all the combinations

--- ui/app.py
+++ ui/app.py
@@ -1,3 +1,4 @@
+import random
 import streamlit as st
 from itertools import permutations
 
@@ -8,14 +9,14 @@ def get_combinations(number):
 def main():
     st.title("4-Digit Combinations")
 
-    number_input = st.number_input("Enter a 4-digit number:", min_value=1000, max_value=9999, step=1)
-
+    number_input = st.number_input("Enter a 4-digit number:", min_value=0000, max_value=9999, step=1, value=8888)
     if st.button("Get Combinations"):
         combinations = get_combinations(number_input)
         st.write(f"All possible combinations of {number_input}:")
         for combination in combinations:
             st.write("".join(str(d) for d in combination))
-
+        random_number = "".join(str(d) for d in random.choice(combinations))
+        st.write(f"<span style='font-size: 24px'>Your lucky number for 4D today is <b>{random_number}</b></span>", unsafe_allow_html=True)
 
 if __name__=='__main__': 
 
